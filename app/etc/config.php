32fe2ec668e9948feed983817c1b8664bc1a2d16
amgawish-x <ahmed.gawish@scandiweb.com>
2025-06-09 12:37:19 +0300

    Revert "Merge branch 'No-2FA' into dev"
    This reverts commit c7e67c060a48a526e75b58b34d50c1d8993169d1, reversing
    changes made to dac6a1a7312f6a09c16d8e072999178ac5f1f720.

--- app/etc/config.php
+++ app/etc/config.php
@@ -374,7 +374,6 @@ return [
         'Magento_WishlistGraphQl' => 1,
         'Magento_WishlistSampleData' => 1,
         'MarkShust_SimpleData' => 1,
-        'MarkShust_DisableTwoFactorAuth' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeCustomerBalance' => 1,
         'PayPal_BraintreeGiftCardAccount' => 1,
